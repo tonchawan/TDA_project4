6421f6fc9ba3ba9abe3551a1691bc37a7123c29e
tonchawan <tonchawan50@gmail.com>
2023-01-23 02:24:58 +0700

    start auth

--- heal_django/settings.py
+++ heal_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'heal',
+    'accounts'
 
 ]
 
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
