72c5a9d1c9b3d9bb228e676b46d613d90c1df88f
tonchawan <tonchawan50@gmail.com>
2023-01-24 11:26:04 +0700

    after present

--- heal/views.py
+++ heal/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Accounts, InsuranceProducts, Recives, Users
 from .forms import AccountForm, ReciveForm
 
+
 # Create your views here.
 
 # Show all User data
@@ -11,6 +12,7 @@ def users_view(request):
     return render(request, 'heal/users_view.html', context)
 
 # Show all Accounts data
+
 def accounts_view(request):
     accounts = Accounts.objects.all()
     context = {'accounts': accounts}
@@ -23,6 +25,7 @@ def insuranceproducts_view(request):
     return render(request, 'heal/products.html', context)
 
 # Show all Recives data
+
 def recives_view(request):
     recives = Recives.objects.all()
     context = {'recives': recives}
@@ -30,21 +33,25 @@ def recives_view(request):
 
 
 # Show a User detail
+
 def user_detail(request, pk):
     user = Users.objects.get(id=pk)
     return render(request, 'heal/user_detail.html', {'user':user})
 
 # Show a Account detail
+
 def account_detail(request, pk):
     account = Accounts.objects.all()
     return render(request, 'heal/account_detail.html', {'account':account})
 
 # Show a product detail
+
 def product_detail(request, pk):
     product = InsuranceProducts.objects.get(id=pk)
     return render(request, 'heal/product_detail.html', {'product': product})
 
 # Show a recive detail
+
 def recive_detail(request, pk):
     recive = Recives.objects.get(id=pk)
     return render(request, 'heal/recive_detail.html', {'recive':recive })
@@ -62,6 +69,7 @@ def account_create(request):
     return render(request, 'heal/account_form.html', {'form': form})
 
 #Create recive
+
 def product_buy(request, product_id):
     if request.method == 'POST':
         form = ReciveForm(request.POST)
@@ -75,6 +83,7 @@ def product_buy(request, product_id):
     return render(request, 'heal/recive_form.html', {'form': form})
 
 # Edit Account 
+
 def account_update(request, pk ):
     account = Accounts.objects.get(id=pk)
     if request.method == "POST":
@@ -87,6 +96,7 @@ def account_update(request, pk ):
         return render(request, 'heal/account_form.html', {'form':form})
 
 # Edit Recive
+
 def recive_update(request, pk ):
     recive = Recives.objects.get(id=pk)
     if request.method == "POST":
@@ -100,11 +110,13 @@ def recive_update(request, pk ):
 
 
 # Deleate Account
+
 def account_delete(request, pk):
     Accounts.objects.get(id=pk).delete()
     return redirect('users_view')
 
 # Deleate Recive
+
 def recive_delete(request, pk):
     Recives.objects.get(id=pk).delete()
     return redirect('heal/recive_view')
